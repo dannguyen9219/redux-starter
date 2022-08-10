c9e3b7395797ef2325f84785a04a471b530b9bac
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-08-10 18:24:13 -0500

    install redux devtools, redux map webpack

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 // import { createStore } from "redux"; // Deprecated
-import { configureStore } from "@reduxjs/toolkit";
+import { legacy_createStore as createStore} from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+// import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 
-const store = configureStore(
-    {reducer}
+const store = createStore(
+    reducer,
+    devToolsEnhancer({ trace: true })
 );
 
 
