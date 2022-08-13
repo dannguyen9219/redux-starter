50716ead68f958ddcd66bbca0a5c1033896ee08c
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-08-13 09:58:40 -0500

    refactor code - action types, action creators, reducers now in bugs.js

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,7 +1,28 @@
-// Assume that there's an empty array for bugs
-// import { BUG_ADDED, BUG_REMOVE} from './actionTypes';
-import * as actions from './actionTypes';
+// Action types
+const BUG_ADDED = "bugAdded";
+const BUG_REMOVED = "bugRemoved";
+const BUG_RESOLVED = "bugResolved";
 
+// Action Creators
+export const bugAdded = description => ({
+    type: BUG_ADDED,
+    payload: {
+        description: description
+    }
+});
+// Using arrow functions is more discrete.
+
+
+export const bugResolved = id => ({
+    type: BUG_RESOLVED,
+    payload: {
+        id: id
+    }
+});
+// Dispatching an action is not easy, just take a look at index.js when dispatching the action for bugAdded and bugRemoved. You have to type the entire structure of this object. What if there are multiple places where we want to dispatch the same action, when we have to repeat all this structure in multiple places.
+// To improve this, we can create a function that would create this action object for us. We call it an action creator. Then we will just update this function here only once.
+
+// Reducer
 let lastId = 0;
 
 export default function reducer(state = [], action) {
@@ -21,7 +42,7 @@ export default function reducer(state = [], action) {
 
     // Using switch and case
     switch (action.type) {
-        case actions.BUG_ADDED:
+        case BUG_ADDED:
             return [
                 ...state,
                 {
@@ -31,10 +52,10 @@ export default function reducer(state = [], action) {
                 }
             ];
         
-        case actions.BUG_REMOVED:
+        case BUG_REMOVED:
             return state.filter(bug => bug.id !== action.payload.id);
         
-        case actions.BUG_RESOLVED:
+        case BUG_RESOLVED:
             return state.map(bug => 
                 bug.id !== action.payload.id ? bug : {...bug, resolved: true});
                 // takes a bug, if the id of the bug does not equal the action.payload.id, then we just want to return the bug as is. Otherwise, we want to take a copy of this bug with all of its properties, and updating the resolved to true
